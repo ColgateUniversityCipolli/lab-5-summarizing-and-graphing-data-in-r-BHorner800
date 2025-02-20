44cc365cb680d65a10564a13aadad326e86a449d
BHorner800 <benjamin.r.horner@gmail.com>
2025-02-20 15:59:49 -0500

    End of class Lab5 update
    Function works, working on the for loop.

--- Lab5Code.R
+++ Lab5Code.R
@@ -24,15 +24,16 @@ library(tidyverse)
 allentown.data = read.csv("./data/essentia.data.allentown.csv")
 test.data = read.csv("./data/essentia.data.csv")
   
-loud <- allentown.data[["overall_loudness"]]
+feature <- "overall_loudness"
+loud <- allentown.data[[feature]]
 test.data|>
   #Grouping the data by artist
   group_by(artist)|>
   #Summarizing the data (min, lower fence, upper fence, maximum)
-  summarise(min = min(overall_loudness),
-            LF = quantile(overall_loudness, 0.25) - 1.5*IQR(overall_loudness),
-            UF = quantile(overall_loudness, 0.75) + 1.5*IQR(overall_loudness),
-            max = max(overall_loudness)
+  summarise(min = min(get(feature)),
+            LF = quantile(get(feature), 0.25) - 1.5*IQR(get(feature)),
+            UF = quantile(get(feature), 0.75) + 1.5*IQR(get(feature)),
+            max = max(get(feature))
             ) |>
   mutate(out.of.range = loud < min | loud > max)|>
   mutate(unusual = loud < LF | loud > UF)|>
@@ -42,8 +43,15 @@ test.data|>
     .default = "Within Range"
   ))
 
-within.range = function(feature){
-  '''
+
+#Loading the data
+allentown.data = read.csv("./data/essentia.data.allentown.csv")
+other.songs.data = read.csv("./data/essentia.data.csv")
+
+
+#Function to summarize each feature
+within.range = function(feature, allentown.data, test.data){
+  '
   feature -> Out of Range, Outlying, or Within Range
   
   Inputs the feature from all the other songs grouped by artist to find the
@@ -51,20 +59,39 @@ within.range = function(feature){
   Compares it to the allentown values
   Outputs if the data is within range, outlying, or out of range for this
   feature of allentown for each artist
-  '''
-  summarise(min = min(overall_loudness),
-            LF = quantile(overall_loudness, 0.25) - 1.5*IQR(overall_loudness),
-            UF = quantile(overall_loudness, 0.75) + 1.5*IQR(overall_loudness),
-            max = max(overall_loudness)
-  ) |>
-    mutate(out.of.range = loud < min | loud > max)|>
-    mutate(unusual = loud < LF | loud > UF)|>
+  '
+  allentown.feature <- allentown.data[[feature]]
+  test.data|>
+    #Grouping the data by artist
+    group_by(artist)|>
+    #Summarizing the data (min, lower fence, upper fence, maximum)
+    summarise(min = min(get(feature)),
+              LF = quantile(get(feature), 0.25) - 1.5*IQR(get(feature)),
+              UF = quantile(get(feature), 0.75) + 1.5*IQR(get(feature)),
+              max = max(get(feature))
+    ) |>
+    mutate(out.of.range = allentown.feature < min | allentown.feature > max)|>
+    mutate(unusual = allentown.feature < LF | allentown.feature > UF)|>
     mutate(description = case_when(
       out.of.range == TRUE ~ "Out of Range",
       unusual == TRUE ~ "Outlying",
-      .default = "Within Range"))
+      .default = "Within Range"
+    ))
 }
 
 
 
+
+#Looping through each feature of the data
+descriptors.df = data.frame()
+for (feature in colnames(other.songs.data)){
+  #Only evaluating numeric features
+  if (is.numeric(allentown.data[[feature]] == TRUE)){
+    #Running the function
+    result = within.range(feature, allentown.data, other.songs.data)
+    
+  }
   
+}
+
+?merge()
